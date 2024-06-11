016245faa3d2893bbc696e0f7d70678c3c4077a5
Kat Law <kathleen.law@multiverse.io>
2024-06-11 17:25:26 -0400

    loooooop

--- src/shell.c
+++ src/shell.c
@@ -1,10 +1,15 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include "shell.h"
 
 void execute_shell(void) {
     char command[50];
     printf("pcshell> ");
     scanf("%s", command);
-    printf("%s\n", command);
+    while (sizeof(command) > 0) {
+        printf("%s\n", command);
+        printf("pcshell> ");
+        scanf("%s", command);
+    }
 }
