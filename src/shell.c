03498703e2350e40b4c26b3cafd443ee48168025
Kat Law <kathleen.law@multiverse.io>
2024-06-12 10:09:59 -0400

    exit command, adjusting tests

--- src/shell.c
+++ src/shell.c
@@ -7,8 +7,8 @@ void execute_shell(void) {
     char command[50];
     printf("pcshell> ");
     scanf("%s", command);
-    while (sizeof(command) > 0) {
-        printf("%s\n", command);
+    while (strcmp(command, "exit") != 0) {
+        printf("\nhello %s\n", command);
         printf("pcshell> ");
         scanf("%s", command);
     }
